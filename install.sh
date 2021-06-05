792d00d98593ac3e51ee09374c5b9606c8d866fa
Shane Hutter <shane.hutter@serversaustralia.com.au>
2021-06-05 10:30:32 +1000

    Starting fresh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-#  This software requires python >= 3.6
-
-python3 setup.py clean && python3 setup.py bdist && sudo python3 setup.py install
-python3 setup.py clean
-rm -fr build dist
