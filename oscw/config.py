2cf51943b9d0656fa7d3e8edfb10ffcb7050db1f
Shane Hutter <shane.hutter@serversaustralia.com.au>
2021-06-05 11:21:39 +1000

    Adding config file loading

--- oscw/config.py
+++ oscw/config.py
@@ -1,3 +1,42 @@
 #!/usr/bin/env python3
 """OSCW Configuration file parsing
 """
+
+from yaml   import safe_load
+
+class Config():
+    """Load an OSCW configuration file"""
+    def __init__( self, config ):
+        """Init for Config class
+
+        Args:
+            config: (str)   Location for OSCW configuration file
+        """
+        self.config_file = config
+        return
+
+    def __enter__( self ):
+        """Load config upon entering `with`"""
+        self.load_conf( self.config_file )
+        return self
+
+    def __exit__( self, *exception ):
+        """Close the config upon exiting `with`"""
+        self.close_conf()
+        return
+
+    def load_conf( self , config ):
+        """Load the configuration file"""
+        self._conf = open(
+            self.config_file,
+            "r",
+            )
+        self.config = safe_load(
+            self._conf.read()
+            )
+        return
+
+    def close_conf( self ):
+        """Close the config file"""
+        self._conf.close()
+        return
